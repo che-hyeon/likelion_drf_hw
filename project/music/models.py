2bab0e602fced232b27fe877438284bed3ef922f
che-hyeon <ghkdcogus03@naver.com>
2024-07-09 16:03:14 +0900

    방학 2주차 과제

--- project/music/models.py
+++ project/music/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+
+def image_upload_path(instance, filename):
+    return f'{instance.pk}/{filename}'
 
 class Singer(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30)
     content = models.TextField(max_length=200)
     debut = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    tag = models.ManyToManyField(Tag, blank=True)
+    image = models.ImageField(upload_to=image_upload_path, blank=True, null=True)
 
 class Song(models.Model):
     id = models.AutoField(primary_key=True)
--- project/music/serializers.py
+++ project/music/serializers.py
@@ -3,6 +3,8 @@ from .models import *
 
 class SingerSerializer(serializers.ModelSerializer):
     id = serializers.CharField(read_only=True)
+    created_at = serializers.CharField(read_only=True)
+    updated_at = serializers.CharField(read_only=True)
 
     songs = serializers.SerializerMethodField(read_only=True)
 
@@ -10,12 +12,25 @@ class SingerSerializer(serializers.ModelSerializer):
         serializer = SongSerializer(instance.songs, many = True)
         return serializer.data
 
+    tag = serializers.SerializerMethodField()
+    def get_tag(self, instance):
+        tags = instance.tag.all()
+        return [tag.name for tag in tags]
     class Meta:
         model = Singer
-        fields = ['id', 'name', 'content', 'debut', 'songs']
+        # fields = ['id', 'name', 'content', 'debut', 'songs']
+        fields = '__all__'
+
+    image = serializers.ImageField(use_url = True, required=False)
 
 class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
         fields = '__all__'
         read_only_fields = ['singer']
+
+class TagSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tag
+        fields = '__all__'
